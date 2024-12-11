cbfda4b4d20d6704dddc99b16b026522b3a0f7df
Krupesh Soni <117522900+thekrupeshsoni1001@users.noreply.github.com>
2024-12-11 18:09:35 +0530

    Add files via upload

---
+++ frontend/src/hooks/useGetAllRecruiterJobs.jsx
@@ -0,0 +1,24 @@
+import { setAllRecruiterJobs } from '@/redux/jobSlice'
+import { JOB_API_END_POINT } from '@/utils/constant'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+const useGetAllRecruiterJobs = () => {
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        const fetchAllRecruiterJobs = async () => {
+            try {
+                const res = await axios.get(`${JOB_API_END_POINT}/getrecruiterjobs`,{withCredentials:true});
+                if(res.data.success){
+                    dispatch(setAllRecruiterJobs(res.data.jobs));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchAllRecruiterJobs();
+    },[])
+}
+
+export default useGetAllRecruiterJobs
